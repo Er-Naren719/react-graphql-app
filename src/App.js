27c7082e804ba9bb4b810a123da9251d0d2b845f
Naren <patilnarendra3@gmail.com>
2020-09-24 12:27:46 +0530

    feat: builded an app with React.js and GraphQL

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import Transactions from './components/transactions/transactions';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App has-background-light">
+      <div className="container is-fluent">
+        <div className="columns">
+          <div className="column">
+            <Transactions />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
